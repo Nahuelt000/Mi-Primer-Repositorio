e72a93f4ed5c7fd38a286f598a59a63581fb4306
Nahuel <tacachoguille@gmail.com>
2023-09-05 23:14:25 -0300

    se agrego getter y setter de los atributos de calculadora

--- src/ar/unlam/dominio/Calculadora.java
+++ src/ar/unlam/dominio/Calculadora.java
@@ -23,4 +23,17 @@ public class Calculadora {
 		double multiplicacion = this.numeroUno * this.numeroDos;
 		return multiplicacion;
 	}
+	public double getNumeroUno() {
+		return numeroUno;
+	}
+	public void setNumeroUno(double numeroUno) {
+		this.numeroUno = numeroUno;
+	}
+	public double getNumeroDos() {
+		return numeroDos;
+	}
+	public void setNumeroDos(double numeroDos) {
+		this.numeroDos = numeroDos;
+	}
+	
 }
