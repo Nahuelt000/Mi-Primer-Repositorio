55bb36b7af25f587be65d4a84ab26c47b287d017
Nahuel <tacachoguille@gmail.com>
2023-09-05 22:58:23 -0300

    Se agrego atributos , constructor y getter

--- src/ar/unlam/dominio/Calculadora.java
+++ src/ar/unlam/dominio/Calculadora.java
@@ -1,5 +1,26 @@
 package ar.unlam.dominio;
 
 public class Calculadora {
-
+	private double numeroUno;
+	private double numeroDos;
+	public Calculadora(double numeroUno, double numeroDos) {
+		this.numeroUno = numeroUno;
+		this.numeroDos = numeroDos;
+	}
+	public double iniciarSuma() {
+		double suma = this.numeroUno + numeroDos;
+		return suma;
+	}
+	public double iniciarResta() {
+		double resta = this.numeroUno - this.numeroDos;
+		return resta;
+	}
+	public double iniciarDivision() {
+		double division = this.numeroUno / this.numeroDos;
+		return division;
+	}
+	public double iniciarMultiplicacion() {
+		double multiplicacion = this.numeroUno * this.numeroDos;
+		return multiplicacion;
+	}
 }
